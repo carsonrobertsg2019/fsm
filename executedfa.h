787447921dcec6a888d666432c6fe32bff59eb66
carsonrobertsg2019 <104335006+carsonrobertsg2019@users.noreply.github.com>
2022-07-12 16:54:42 -0700

    Completed functionality of computation
    In the second to last commit, there was a header file created that was more or less blank, for the program to use in order to run through the input DFA based on an input string and in this commit, the functionality has been implemented.

--- executedfa.h
+++ executedfa.h
@@ -1,4 +1,17 @@
 #ifndef __EXECUTEDFA_H__
 #define __EXECUTEDFA_H__
 
+#include <iostream>
+#include <vector>
+#include "dfaparser.h"
+using namespace std;
+
+class execution
+{
+public:
+	int findCurrentStateInList(DfaParser M, string w_ch);
+	int findTransitionInList(DfaParser M, int i, string w_ch);
+	bool startExecution(string w, DfaParser M);
+};
+
 #endif
--- fsm.cpp
+++ fsm.cpp
@@ -3,11 +3,12 @@
 #include <string>
 #include <vector>
 #include "dfaparser.h"
+#include "executedfa.h"
 using namespace std;
 
-//************************************************************************************************************************************************************************************************
-//DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING
-//************************************************************************************************************************************************************************************************
+//***********************************************************************************************************************************************************************************
+//DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING DFA PARSING
+//***********************************************************************************************************************************************************************************
 
 int DfaParser::findStateInListOfStates(string state1)
 {
@@ -365,19 +366,68 @@ void DfaParser::printF(DfaParser dfaparser)
 
 void DfaParser::debug(DfaParser dfaparser, string type)
 {
-	if (type == "PARSING")
-	{
-		printQ(dfaparser);
-		printSig(dfaparser);
-		printDelta(dfaparser);
-		printSS(dfaparser);
-		printF(dfaparser);
-	}
-	else if (type == "INPUT_STRING")
-	{
+	cout << "PARSING" << endl;
+	printQ(dfaparser);
+	printSig(dfaparser);
+	printDelta(dfaparser);
+	printSS(dfaparser);
+	printF(dfaparser);
+}
 
+//***********************************************************************************************************************************************************************************
+//DFA COMPUTATION DFA COMPUTATION DFA COMPUTATION DFA COMPUTATION DFA COMPUTATION DFA COMPUTATION DFA COMPUTATION DFA COMPUTATION DFA COMPUTATION DFA COMPUTATION DFA COMPUTATION DFA
+//***********************************************************************************************************************************************************************************
+
+int execution::findCurrentStateInList(DfaParser M, string currentState)
+{
+	for (int i = 0; i < M.listOfStates.size(); i++)
+	{
+		if (currentState == M.listOfStates[i]->stateName)
+		{
+			return i;
+		}
 	}
-	else cout << "incorrect debug type";
+	return -1;
+}
+
+int execution::findTransitionInList(DfaParser M, int i, string w_ch)
+{
+	for (int j = 0; j < M.listOfStates[i]->listOfTransitions.size(); j++)
+	{
+		if (w_ch == M.listOfStates[i]->listOfTransitions[j]->transitionInput)
+		{
+			return j;
+		}
+	}
+	return -1;
+}
+
+bool execution::startExecution(string w, DfaParser M)
+{
+	string currentState = M.startState;
+	string nextState = "";
+	while (w.length() != 0)
+	{
+		string w_ch = w.substr(0, 1);
+		w = w.substr(1, w.length());
+		int i = findCurrentStateInList(M, currentState);
+		int j = findTransitionInList(M, i, w_ch);
+		if (i == -1 || j == -1)
+		{
+			cout << "something went wrong, but that's okay ! you can figure it out, just breathe.";
+			exit(-1);
+		}
+		currentState = M.listOfStates[i]->listOfTransitions[j]->transitionStateName;
+	}	
+
+	for (int i = 0; i < M.finalStates.size(); i++)
+	{
+		if (M.finalStates[i] == currentState)
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 int main()
@@ -435,9 +485,16 @@ int main()
 		cin >> userIn;
 		userIn = tolower(userIn);
 
+		string w;
 		switch (userIn) {
 		case 'w':
-			cout << "run M on w";
+			cout << "run M on w" << endl;
+			execution executor;
+			cin >> w;
+			if (executor.startExecution(w, dfaparser))
+				cout << endl << "M accepts w";
+			else
+				cout << endl << "M does not accept w";
 			break;
 		case 'q':
 			cout << "Goodbye.";
@@ -446,42 +503,4 @@ int main()
 			cout << "Invalid input, please try again.";
 		}
 	}
-
-	/*
-	i just think this looks neat:
-
-						           .='  ' .`/,/!(=)Zm.
-					 .._,,._..  ,-`- `,\ ` -` -`\\7//WW.
-				,v=~/.-,-\- -!|V-s.)iT-|s|\-.'   `///mK%.
-			  v!`i!-.e]-g`bT/i(/[=.Z/m)K(YNYi..   /-]i44M.
-			v`/,`|v]-DvLcfZ/eV/iDLN\D/ZK@%8W[Z..   `/d!Z8m
-		   //,c\(2(X/NYNY8]ZZ/bZd\()/\7WY%WKKW)   -'|(][%4.
-		 ,\\i\c(e)WX@WKKZKDKWMZ8(b5/ZK8]Z7%ffVM,   -.Y!bNMi
-		 /-iit5N)KWG%%8%%%%W8%ZWM(8YZvD)XN(@.  [   \]!/GXW[
-		/ ))G8\NMN%W%%%%%%%%%%8KK@WZKYK*ZG5KMi,-   vi[NZGM[
-	   i\!(44Y8K%8%%%**~YZYZ@%%%%%4KWZ/PKN)ZDZ7   c=//WZK%!
-	  ,\v\YtMZW8W%%f`,`.t/bNZZK%%W%%ZXb*K(K5DZ   -c\\/KM48
-	  -|c5PbM4DDW%f  v./c\[tMY8W%PMW%D@KW)Gbf   -/(=ZZKM8[
-	  2(N8YXWK85@K   -'c|K4/KKK%@  V%@@WD8e~  .//ct)8ZK%8`
-	  =)b%]Nd)@KM[  !'\cG!iWYK%%|   !M@KZf    -c\))ZDKW%`
-	  YYKWZGNM4/Pb  '-VscP4]b@W%     'Mf`   -L\///KM(%W!
-	  !KKW4ZK/W7)Z. '/cttbY)DKW%     -`  .',\v)K(5KW%%f
-	  'W)KWKZZg)Z2/,!/L(-DYYb54%  ,,`, -\-/v(((KK5WW%f
-	   \M4NDDKZZ(e!/\7vNTtZd)8\Mi!\-,-/i-v((tKNGN%W%%
-	   'M8M88(Zd))///((|D\tDY\\KK-`/-i(=)KtNNN@W%%%@%[
-		!8%@KW5KKN4///s(\Pd!ROBY8/=2(/4ZdzKD%K%%%M8@%%
-		 '%%%W%dGNtPK(c\/2\[Z(ttNYZ2NZW8W8K%%%%YKM%M%%.
-		   *%%W%GW5@/%!e]_tZdY()v)ZXMZW%W%%%*5Y]K%ZK%8[
-			'*%%%%8%8WK\)[/ZmZ/Zi]!/M%%%%@f\ \Y/NNMK%%!
-			  'VM%%%%W%WN5Z/Gt5/b)((cV@f`  - |cZbMKW%%|
-				 'V*M%%%WZ/ZG\t5((+)L\'-,,/  -)X(NWW%%
-					  `~`MZ/DZGNZG5(((\,    ,t\\Z)KW%@
-						 'M8K%8GN8\5(5///]i!v\K)85W%%f
-						   YWWKKKKWZ8G54X/GGMeK@WM8%@
-							!M8%8%48WG@KWYbW%WWW%%%@
-							  VM%WKWK%8K%%8WWWW%%%@`
-								~*%%%%%%W%%%%%%%@~
-								   ~*MM%%%%%%@f`
-									   '''''
-	*/
 }
